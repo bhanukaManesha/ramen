b17d91870ab6205b4a7a36465dd265ec2db496e1
bhanukaManesha <bhanukamgamage@gmail.com>
2020-08-24 15:43:01 +0800

    Update common file 🎨

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 source activate pytorch_p36
-ROOT=/home/ubuntu/ramen
-PROJECT_ROOT=/home/ubuntu/ramen
+
+# Using the ebs storage
+# ROOT=/home/ubuntu/ramen
+# PROJECT_ROOT=/home/ubuntu/ramen
+
+# Using efs mount storage
+ROOT=/mnt/efs/ramen
+PROJECT_ROOT=/mnt/efs/ramen
