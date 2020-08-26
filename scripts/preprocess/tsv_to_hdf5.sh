54ce276a4ca3ab3302a40ff0934249654c24da36
bhanukaManesha <bhanukamgamage@gmail.com>
2020-08-26 09:41:54 +0800

    Update tsv script to extract TDIUC features 🎨

--- scripts/preprocess/tsv_to_hdf5.sh
+++ scripts/preprocess/tsv_to_hdf5.sh
@@ -8,7 +8,17 @@ RESULTS_ROOT=${PROJECT_ROOT}/${DATASET}_results
 #python preprocess/tsv_to_hdf5.py --data_root ${DATA_ROOT} --split trainval
 # python preprocess/tsv_to_hdf5.py --data_root ${DATA_ROOT} --split test2015
 
-ln -s ${DATA_ROOT}/features/trainval.hdf5 ${DATA_ROOT}/features/train.hdf5
-ln -s ${DATA_ROOT}/features/trainval.hdf5 ${DATA_ROOT}/features/val.hdf5
-ln -s ${DATA_ROOT}/features/trainval_ids_map.json ${DATA_ROOT}/features/train_ids_map.json
-ln -s ${DATA_ROOT}/features/trainval_ids_map.json ${DATA_ROOT}/features/val_ids_map.json
+# For TDIUC
+# python preprocess/tsv_to_hdf5.py --data_root ${DATA_ROOT} --split trainval --tdiuc
+# python preprocess/tsv_to_hdf5.py --data_root ${DATA_ROOT} --split test2015 --tdiuc
+
+# ln -s ${DATA_ROOT}/features/trainval.hdf5 ${DATA_ROOT}/features/train.hdf5
+# ln -s ${DATA_ROOT}/features/trainval.hdf5 ${DATA_ROOT}/features/val.hdf5
+# ln -s ${DATA_ROOT}/features/trainval_ids_map.json ${DATA_ROOT}/features/train_ids_map.json
+# ln -s ${DATA_ROOT}/features/trainval_ids_map.json ${DATA_ROOT}/features/val_ids_map.json
+
+# For TDIUC
+ln -s ${DATA_ROOT}/features/traintdiuc.hdf5 ${DATA_ROOT}/TDIUC/features/train.hdf5
+ln -s ${DATA_ROOT}/features/valtdiuc.hdf5 ${DATA_ROOT}/TDIUC/features/val.hdf5
+ln -s ${DATA_ROOT}/features/traintdiuc_ids_map.json ${DATA_ROOT}/TDIUC/features/train_ids_map.json
+ln -s ${DATA_ROOT}/features/valtdiuc_ids_map.json ${DATA_ROOT}/TDIUC/features/val_ids_map.json
