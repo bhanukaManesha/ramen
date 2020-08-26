d80156e0cccb778f7ab93a466c061cafe1b0335f
bhanukaManesha <bhanukamgamage@gmail.com>
2020-08-26 09:42:20 +0800

    Add commands used to extract from feature extractor ✨

--- scripts/TDIUC/ramen_TDIUC.sh
+++ scripts/TDIUC/ramen_TDIUC.sh
@@ -7,6 +7,10 @@ DATA_SET=TDIUC
 TEST_DATASET=TDIUC
 DATA_ROOT=${PROJECT_ROOT}/dataset/${DATA_SET}
 
+# Commands to extract TDIUC features in the feature extractor (will not work here)
+# ./tools/generate_tsv.py --split tdiuc_train --cfg experiments/cfgs/faster_rcnn_end2end_resnet.yml --def models/vg/ResNet-101/faster_rcnn_end2end_final/test.prototxt --out traintdiuc_resnet101_faster_rcnn_genome.tsv --net data/faster_rcnn_models/resnet101_faster_rcnn_final.caffemodel
+# ./tools/generate_tsv.py --split tdiuc_val --cfg experiments/cfgs/faster_rcnn_end2end_resnet.yml --def models/vg/ResNet-101/faster_rcnn_end2end_final/test.prototxt --out valtdiuc_resnet101_faster_rcnn_genome.tsv --net data/faster_rcnn_models/resnet101_faster_rcnn_final.caffemodel
+
 # Create dictionary and compute GT answer scores
 python preprocess/create_dictionary.py --data_root ${DATA_ROOT}
 python preprocess/compute_softscore.py --data_root ${DATA_ROOT}
