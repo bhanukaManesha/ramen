d47b4798627ec03c223abc796a66aa1b7fc5ed34
bhanukaManesha <bhanukamgamage@gmail.com>
2020-08-29 05:55:27 +0800

    Update common.sh 🎨

--- scripts/common.sh
+++ scripts/common.sh
@@ -8,3 +8,7 @@ source activate pytorch_p36
 # Using efs mount storage
 ROOT=/mnt/efs/ramen
 PROJECT_ROOT=/mnt/efs/ramen
+
+# Using local mac
+#ROOT=/Users/bhanukagamage/Dropbox/Monash/Thesis/ramen
+#PROJECT_ROOT=/Users/bhanukagamage/Dropbox/Monash/Thesis/ramen
