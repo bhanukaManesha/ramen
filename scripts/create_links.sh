2be72358a7ca169963337efd319cbfc90019b3e3
bhanukaManesha <bhanukamgamage@gmail.com>
2020-09-29 13:04:34 +0800

    Add create links ✨

--- scripts/create_links.sh
+++ scripts/create_links.sh
@@ -18,3 +18,58 @@ ln -s ../../CLEVR/features/test_ids_map.json test_ids_map.json
 
 cd ${PROJECT_ROOT}/dataset/CLEVR_CoGenTA/glove
 ln -s ../../glove/glove.6B.300d.txt glove.6B.300d.txt
+
+
+# CLEVR_CoGenTA
+cd ${PROJECT_ROOT}/dataset/CLEVR_CoGenTB/features
+
+ln -s ../../CLEVR/features/train.hdf5 train.hdf5
+ln -s ../../CLEVR/features/train_ids_map.json train_ids_map.json
+ln -s ../../CLEVR/features/train_scenes_with_bb.json train_scenes_with_bb.json
+
+ln -s ../../CLEVR/features/val.hdf5 val.hdf5
+ln -s ../../CLEVR/features/val_ids_map.json val_ids_map.json
+ln -s ../../CLEVR/features/val_scenes_with_bb.json val_scenes_with_bb.json
+
+ln -s ../../CLEVR/features/test.hdf5 test.hdf5
+ln -s ../../CLEVR/features/test_ids_map.json test_ids_map.json
+
+cd ${PROJECT_ROOT}/dataset/CLEVR_CoGenTB/glove
+ln -s ../../glove/glove.6B.300d.txt glove.6B.300d.txt
+
+
+# CLEVR_Human
+cd ${PROJECT_ROOT}/dataset/CLEVR_Human/features
+
+ln -s ../../CLEVR/features/train.hdf5 train.hdf5
+ln -s ../../CLEVR/features/train_ids_map.json train_ids_map.json
+ln -s ../../CLEVR/features/train_scenes_with_bb.json train_scenes_with_bb.json
+
+ln -s ../../CLEVR/features/val.hdf5 val.hdf5
+ln -s ../../CLEVR/features/val_ids_map.json val_ids_map.json
+ln -s ../../CLEVR/features/val_scenes_with_bb.json val_scenes_with_bb.json
+
+ln -s ../../CLEVR/features/test.hdf5 test.hdf5
+ln -s ../../CLEVR/features/test_ids_map.json test_ids_map.json
+
+cd ${PROJECT_ROOT}/dataset/CLEVR_Human/glove
+ln -s ../../glove/glove.6B.300d.txt glove.6B.300d.txt
+
+# VQA
+cd ${PROJECT_ROOT}/dataset/CLEVR_Human/features
+
+ln -s ../../features/valtdiuc_ids_map.json val_ids_map.json
+
+ln -s ../../CLEVR/features/train.hdf5 train.hdf5
+ln -s ../../CLEVR/features/train_ids_map.json train_ids_map.json
+ln -s ../../CLEVR/features/train_scenes_with_bb.json train_scenes_with_bb.json
+
+ln -s ../../CLEVR/features/val.hdf5 val.hdf5
+ln -s ../../CLEVR/features/val_ids_map.json val_ids_map.json
+ln -s ../../CLEVR/features/val_scenes_with_bb.json val_scenes_with_bb.json
+
+ln -s ../../CLEVR/features/test.hdf5 test.hdf5
+ln -s ../../CLEVR/features/test_ids_map.json test_ids_map.json
+
+cd ${PROJECT_ROOT}/dataset/CLEVR_Human/glove
+ln -s ../../glove/glove.6B.300d.txt glove.6B.300d.txt
