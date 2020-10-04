c0622538c018cbc887859e37371f659a5412037a
bhanukaManesha <bhanukamgamage@gmail.com>
2020-10-05 04:00:12 +0800

    Fix 🐛 in script

--- scripts/mynet_VQACP.sh
+++ scripts/mynet_VQACP.sh
@@ -6,11 +6,9 @@ cd ${PROJECT_ROOT}
 DATA_SET=VQACP
 DATA_ROOT=${PROJECT_ROOT}/dataset/${DATA_SET}
 
-# Convert to VQA2-like format
-python -u preprocess/convert_from_clevr_to_vqa_format.py --data_root ${DATA_ROOT}
 # Create dictionary and compute GT answer scores
 python preprocess/create_dictionary.py --data_root ${DATA_ROOT}
-python preprocess/compute_softscore.py --data_root ${DATA_ROOT}
+python preprocess/compute_softscore.py --data_root ${DATA_ROOT} --min_occurrence 9
 
 RESULTS_ROOT=${PROJECT_ROOT}/results/${DATA_SET}_results
 mkdir -p ${RESULTS_ROOT}
