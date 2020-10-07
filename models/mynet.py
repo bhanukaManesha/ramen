a67b79c0e8a68b5db4889811a1eba7d86b4bb9bb
bhanukaManesha <bhanukamgamage@gmail.com>
2020-10-07 10:51:37 +0800

    Add CVQA scripts ✨

--- models/mynet.py
+++ models/mynet.py
@@ -18,7 +18,7 @@ class Mynet(nn.Module):
                                        dropout_before_rnn=config.question_dropout_before_rnn,
                                        dropout_after_rnn=config.question_dropout_after_rnn)
 
-        clf_in_size = 15 * 256
+        clf_in_size = 36 * 256
         classifier_layers = []
         for ix, size in enumerate(config.classifier_sizes):
             in_s = clf_in_size if ix == 0 else config.classifier_sizes[ix - 1]
