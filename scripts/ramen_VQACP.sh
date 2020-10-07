561a1a5b26d1fa3bedf1dacb9cfa47c9c32d0384
Student <bhanukamgamage@gmail.com>
2020-10-07 14:40:37 +0800

    VQACP script

--- scripts/ramen_VQACP.sh
+++ scripts/ramen_VQACP.sh
@@ -3,24 +3,25 @@ set -e
 source scripts/common.sh
 cd ${PROJECT_ROOT}
 
-DATA_SET=VQACP
-DATA_ROOT=/hdd/robik/${DATA_SET}
+DATA_SET=VQACP2
+DATA_ROOT=${PROJECT_ROOT}/dataset/${DATA_SET}
 
 # Create dictionary and compute GT answer scores
 #python preprocess/create_dictionary.py --data_root ${DATA_ROOT}
 #python preprocess/compute_softscore.py --data_root ${DATA_ROOT} --min_occurrence 9
 
 # Train the model
-RESULTS_ROOT=/hdd/robik/${DATA_SET}_results
+RESULTS_ROOT=${PROJECT_ROOT}/results/${DATA_SET}_results
 mkdir -p ${RESULTS_ROOT}
 MODEL=Ramen
-EXPT_NAME=${MODEL}_${DATA_SET}
+EXPT_NAME=${MODEL}_${DATA_SET}_np
 
 python -u run_network.py \
 --data_set ${DATA_SET} \
 --data_root ${DATA_ROOT} \
 --expt_name ${EXPT_NAME} \
 --model ${MODEL} \
+--batch_size 128 \
 --words_dropout 0.5 \
 --question_dropout_after_rnn 0.5 \
---h5_prefix use_split > ${RESULTS_ROOT}/${EXPT_NAME}.log
+--h5_prefix use_split 2>&1 | tee ${RESULTS_ROOT}/${EXPT_NAME}.log
