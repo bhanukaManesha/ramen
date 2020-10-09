c42d601de1ae1d2fe699ea7dd13cce5631b58132
Student <bhanukagamage@gmail.com>
2020-10-09 10:04:18 +0800

    Update VQA script

--- scripts/ramen_VQA2.sh
+++ scripts/ramen_VQA2.sh
@@ -5,17 +5,17 @@ cd ${PROJECT_ROOT}
 
 
 DATA_SET=VQA2
-DATA_ROOT=${ROOT}/${DATA_SET}
+DATA_ROOT=${PROJECT_ROOT}/dataset/${DATA_SET}
 
 # Create dictionary and compute GT answer scores
-#python preprocess/create_dictionary.py --data_root ${DATA_ROOT}
-#python preprocess/compute_softscore.py --data_root ${DATA_ROOT} --min_occurrence 9
+# python preprocess/create_dictionary.py --data_root ${DATA_ROOT}
+# python preprocess/compute_softscore.py --data_root ${DATA_ROOT} --min_occurrence 9
 
 # Train the model
-RESULTS_ROOT=/hdd/robik/${DATA_SET}_results
+RESULTS_ROOT=${PROJECT_ROOT}/results/${DATA_SET}_results
 mkdir -p ${RESULTS_ROOT}
 MODEL=Ramen
-EXPT_NAME=${MODEL}_${DATA_SET}
+EXPT_NAME=${MODEL}_${DATA_SET}_baseline
 
 python -u run_network.py \
 --data_set ${DATA_SET} \
@@ -23,7 +23,7 @@ python -u run_network.py \
 --expt_name ${EXPT_NAME} \
 --model ${MODEL} \
 --train_split trainval \
---test_split test_dev \
+--test_split test \
 --words_dropout 0.5 \
 --question_dropout_after_rnn 0.5 \
---h5_prefix use_split > ${RESULTS_ROOT}/${EXPT_NAME}.log
+--h5_prefix use_split  2>&1 | tee ${RESULTS_ROOT}/${EXPT_NAME}.log
