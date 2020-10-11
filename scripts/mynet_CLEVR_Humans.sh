b4c540c09cbdcbb0b0fc6a6799fd4b59031935c5
bhanukaManesha <bhanukamgamage@gmail.com>
2020-10-11 10:48:03 +0800

    Add new script to preprocess CLEVR-Humans ✨

--- scripts/mynet_CLEVR_Humans.sh
+++ scripts/mynet_CLEVR_Humans.sh
@@ -7,7 +7,7 @@ DATA_SET=CLEVR_Humans
 DATA_ROOT=${PROJECT_ROOT}/dataset/${DATA_SET}
 
 # Convert to VQA2-like format
-python -u preprocess/convert_from_clevr_to_vqa_format.py --data_root ${DATA_ROOT}
+python -u preprocess/convert_from_clevr_humans_to_vqa_format.py --data_root ${DATA_ROOT}
 # Create dictionary and compute GT answer scores
 python preprocess/create_dictionary.py --data_root ${DATA_ROOT}
 python preprocess/compute_softscore.py --data_root ${DATA_ROOT}
