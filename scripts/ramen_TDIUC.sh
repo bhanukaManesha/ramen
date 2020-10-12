e1863683b461173629d8e780879615f4ae4ea9ef
Student <bhanukamgamage@gmail.com>
2020-10-12 14:20:14 +0800

    Add new training scripts

--- scripts/ramen_TDIUC.sh
+++ scripts/ramen_TDIUC.sh
@@ -8,8 +8,8 @@ DATA_SET=TDIUC
 DATA_ROOT=${PROJECT_ROOT}/dataset/${DATA_SET}
 
 # Create dictionary and compute GT answer scores
- python preprocess/create_dictionary.py --data_root ${DATA_ROOT}
- python preprocess/compute_softscore.py --data_root ${DATA_ROOT} --min_occurrence 0 --tdiuc
+#  python preprocess/create_dictionary.py --data_root ${DATA_ROOT}
+#  python preprocess/compute_softscore.py --data_root ${DATA_ROOT} --min_occurrence 0 --tdiuc
 
 # Train the model
 RESULTS_ROOT=${PROJECT_ROOT}/results/${DATA_SET}_results
